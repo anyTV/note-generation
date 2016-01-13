4b9d31367cc79369112e0a82d7201e5278101a5d
rohan <rohan@rohans-MacBook-Pro.local>
2016-01-13 23:57:47 +0800

    first commit

---
+++ config/router.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const importer = require('anytv-node-importer');
+
+
+
+module.exports = (router) => {
+    const __ = importer.dirloadSync(__dirname + '/../controllers');
+
+    router.del = router.delete;
+
+    router.get('/', (req, res) => {
+        res.render('index');
+    });
+
+    router.get('/user/:id', __.user.get_user);
+
+    router.all('*', (req, res) => {
+        res.status(404)
+            .send({message: 'Nothing to do here.'});
+    });
+
+    return router;
+};
